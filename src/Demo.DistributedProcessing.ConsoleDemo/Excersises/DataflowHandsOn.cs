f1bfbd039a5b568872d457b65161ab2683238d97
I40497 <surendra.shrestha@verisk.com>
2025-07-23 14:40:31 +0545

    add excerise template for dataflow blocks

---
+++ src/Demo.DistributedProcessing.ConsoleDemo/Excersises/DataflowHandsOn.cs
@@ -0,0 +1,18 @@
+﻿namespace Demo.DistributedProcessing.ConsoleDemo.Excersises;
+
+// Create a data flow blocks for a car factory
+internal class DataflowHandsOn
+{
+    // Create data flow block that will create cars using the dataflow blocks. You can define each blocks as you fit,
+    // you can use the following blocks as a guideline
+    // Create frame
+    // Put engine
+    // Add interior
+    // Add paint
+    // Delivery/ready
+
+    public static Task Run()
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Demo.DistributedProcessing.ConsoleDemo/Program.cs
+++ src/Demo.DistributedProcessing.ConsoleDemo/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Demo.DistributedProcessing.ConsoleDemo.Demos;
+using Demo.DistributedProcessing.ConsoleDemo.Excersises;
 
 //TaskParallelDemo.Run();
 //ParallelLinqDemo.Run();
 //await DataflowDemo.Run();
 //await DistributedProgramming.Run();
+
+// exercise
+//await DataflowHandsOn.Run();
