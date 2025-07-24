32a48dddbb582bcf4c53a035adeb4b5204d163d2
Poudel, Amrit <I37710@verisk.com>
2025-07-24 09:22:11 +0545

    test

--- src/FileIngestorApp.Benchmark/Program.cs
+++ src/FileIngestorApp.Benchmark/Program.cs
@@ -3,13 +3,15 @@
 #if DEBUG
 
 var summary1 = new FileProcessorBenchmark();
+
+
+Console.WriteLine("Generating Files..");
 summary1.Setup();
 
 
 //Console.WriteLine("Calling.. Legacy");
 //summary1.Legacy();
-//
-//
+
 //Console.WriteLine("Calling..  Optimized");
 //summary1.Optimized();
 
