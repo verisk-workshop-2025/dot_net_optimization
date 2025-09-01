ed29d679c7333ccaf93dd84dd858542af7f9a981
I40497 <surendra.shrestha@verisk.com>
2025-09-01 09:55:46 +0545

    add solution to dataflow blocks excersise

--- src/Demo.DistributedProcessing.ConsoleDemo/Excersises/DataflowHandsOn.cs
+++ src/Demo.DistributedProcessing.ConsoleDemo/Excersises/DataflowHandsOn.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo.DistributedProcessing.ConsoleDemo.Excersises;
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace Demo.DistributedProcessing.ConsoleDemo.Excersises;
 
 // Create a data flow blocks for a car factory
 internal class DataflowHandsOn
@@ -11,8 +13,42 @@ internal class DataflowHandsOn
     // Add paint
     // Delivery/ready
 
-    public static Task Run()
+    public static async Task Run()
     {
-        throw new NotImplementedException();
+        TransformBlock<string, string> createFrameBlock = new(t =>
+        {
+            Console.WriteLine("Creating frame");
+            return "Frame created";
+        });
+        TransformBlock<string, string> putEngineBlock = new(t =>
+        {
+            Console.WriteLine("Putting engine");
+            return "Engine added";
+        });
+        TransformBlock<string, string> addInteriorBlock = new(t =>
+        {
+            Console.WriteLine("Adding interior");
+            return "Interior added";
+        });
+        TransformBlock<string, string> addPaintBlock = new(t =>
+        {
+            Console.WriteLine("Adding paint");
+            return "Painted";
+        });
+        ActionBlock<string> deliverBlock = new(t =>
+        {
+            Console.WriteLine("Delivered");
+        });
+
+        DataflowLinkOptions linkOptions = new() { PropagateCompletion = true };
+
+        createFrameBlock.LinkTo(putEngineBlock, linkOptions);
+        putEngineBlock.LinkTo(addInteriorBlock, linkOptions);
+        addPaintBlock.LinkTo(deliverBlock, linkOptions);
+
+        createFrameBlock.Post("Tesla");
+        createFrameBlock.Complete();
+
+        await deliverBlock.Completion;
     }
 }
